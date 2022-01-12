388f7a9a0d6791cec2988b33b08630f184b0ecd6
Nguyễn Văn Thắng <43449125+NguyenVanThang17110230@users.noreply.github.com>
2022-01-12 20:03:44 +0700

    update

--- client/next.config.js
+++ client/next.config.js
@@ -10,7 +10,7 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://2bc4-115-79-44-251.ngrok.io/api/:path*",
+        destination: "http://localhost:8090/api/:path*",
         // destination: 'https://document-manager-app.herokuapp.com/api/:path*',
       },
     ];
